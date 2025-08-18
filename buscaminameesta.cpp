7608a55a52a0298af0f4db6e67c2a989754962f6
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2025-08-17 19:50:15 -0500

    Update buscaminameesta.cpp

--- buscaminameesta.cpp
+++ buscaminameesta.cpp
@@ -1,4 +1,6 @@
-	#include "iostream"
+	
+//faltan cosas
+#include "iostream"
 	using namespace std;
 	int main() {
 		int arr[10][10] = {0};
@@ -16,10 +18,7 @@
 		}
 		//fila
 		
-	/*	int num=5565;
-		int* puntero = &num;
-		cout << &puntero ;
-		int* q = puntero;*/
+
 		
 		for (int (*y)[10] = arr; y < arr+10; y++)
 		{
@@ -29,42 +28,44 @@
 			{
 				int fila = y - arr;  
 				int col = i - *y;
-				
+
+				if (*i == -1) continue; // no cuente los -1 cuando el i esta ahi AHHHHHHHHHHHHHHHH
+				int contador=0;
 					if (fila > 0) //cuando existe un arriba 
 					{
 						int* cicloarriba = *(arr + fila - 1) + col;
 							if (*cicloarriba==-1)
 							{
-								*i = 1;
+								contador++;
 							}
 						if (col <= 8) {
 							int* cicloarribaderecha = (*(arr + fila - 1) + col) + 1;
 							if (*cicloarribaderecha == -1) {
-								*i = 1;
+								contador++;
 							}
 						}
 						if (col > 0) {
 							int* cicloarribaizquierda = (*(arr + fila - 1) + col) - 1;
 							if (*cicloarribaizquierda == -1) {
-								*i = 1;
+								contador++;
 							}
 						}
 					}
 					if (fila < 9) { // cuando existe un abajo
 						int* cicloabajo = *(arr + fila + 1) + col;
 						if (*cicloabajo == -1) {
-							*i = 1;
-							}
+							contador++;
+						}
 						if (col <= 8) {
 							int* cicloabajoderecha = (*(arr + fila + 1) + col) +1 ;
 							if (*cicloabajoderecha == -1) {
-								*i = 1;
+								contador++;
 							}
 						}
 						if (col > 0) {
 							int* cicloabajoizquierda = (*(arr + fila + 1) + col) -1 ;
 							if (*cicloabajoizquierda == -1) {
-								*i = 1;
+								contador++;
 							}
 						}
 					}
@@ -74,7 +75,7 @@
 						int* derecha = i + 1;
 						if (*derecha == -1)
 						{
-							*i = 1;
+							contador++;
 						}
 					}
 
@@ -82,20 +83,16 @@
 						int* izquierda = i - 1;
 						if (*izquierda == -1)
 						{
-							*i = 1;
+							contador++;
 						}
 					}
+					*i = contador;
 
 
 
 
 
-
-					//casos especiales: primera fila y la ultima fila , al igual que la primera columna y la ultima columna
-					//caso mas especial son las esquinas , no hay casos especiales lptmre
-					/*if (fila == 1) { 
-
-					}*/
+				
 			}
 		}
 		for (int (*p)[10] = arr; p < arr+10; p++){

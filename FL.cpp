66f58535949a8b5b996ee4956a33e8e83da771d1
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2025-08-25 10:13:43 -0500

    Create FL.cpp

---
+++ FL.cpp
@@ -0,0 +1,104 @@
+
+#include "iostream"
+using namespace std;
+
+
+struct Node
+{
+
+    //atributos de cada nodo
+    Node* next;
+    int v;
+    
+    Node(int x) { //constructor 
+        v = x;
+        next = 0; 
+    }
+   
+};
+
+
+class FL
+{
+public:
+    FL() { //constructor
+        head = 0;
+        nelem = 0;
+    } 
+    void push_front(int x);
+    void push_back(int x);
+    void pop_back();
+    void pop_front();
+
+    void print();
+    ~FL(); //destructor
+
+private:
+    Node* head;
+    int nelem;
+};
+
+FL::~FL()
+{
+    delete head;
+}
+
+void FL::push_back(int x) { // agregar al final
+    Node* n = new Node(x);
+    Node** q;
+    for ( q = &head; *q != 0; q = &((*q)->next));
+    *q = n;
+}
+void FL::push_front(int x) { //agregar al inicio
+    Node* n = new Node(x);
+    n->next = head;
+    n->v = x;
+    head = n;
+    nelem++;
+}
+void FL::pop_back() { //eliminar al final
+    
+//con puntero doble
+    Node** y;
+    for (y = &head; (*y)->next !=0; y = &((*y)->next));
+    delete *y;
+    *y = 0;
+  //Creando un tmp auxiliar usando un puntero simple
+  /*  Node* tmp = 0;
+    Node* y;
+    for ( y = head; y->next != 0; y = y->next)
+            tmp = y;
+    tmp->next = 0;
+
+    delete y;*/
+}
+void FL::pop_front() { //eliminar al inicio
+
+    Node* tmp = 0;
+    tmp = head;
+    head = tmp->next;
+    delete tmp;
+}
+
+
+void FL::print()
+{
+    for (Node *i = head; i; i=i->next)
+    {
+        cout << i->v << "->";
+    }
+    cout << "NULL";
+
+
+}
+int main() {
+    FL fl;
+    fl.push_front(5);
+    fl.push_front(4);
+    fl.push_front(3);
+    fl.push_back(8);
+    fl.pop_back();
+    fl.pop_front();
+    fl.print();
+    return 0;
+}

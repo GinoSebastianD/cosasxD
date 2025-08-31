47f36ca27759185c7cfd7c85991279c8c34c1199
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2025-08-30 23:41:20 -0500

    Create Problema2AED.cpp

---
+++ Problema2AED.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+
+int arr[10][10] = { 0 };
+
+void cuentaporfilas(int ini, int fin) {
+    for (int (*y)[10] = arr + ini; y < arr + fin; y++) {
+        for (int* i = *y; i < *(y + 1); i++) {
+            int fila = y - arr;
+            int col = i - *y;
+
+            if (*i == -1) continue;
+            int contador = 0;
+
+            if (fila > 0) {
+                if (*(*(arr + fila - 1) + col) == -1)
+                    contador++;
+                if (col < 10 - 1 && *(*(arr + fila - 1) + col + 1) == -1)
+                    contador++;
+                if (col > 0 && *(*(arr + fila - 1) + col - 1) == -1)
+                    contador++;
+            }
+            if (fila < 10 - 1) {
+                if (*(*(arr + fila + 1) + col) == -1)
+                    contador++;
+                if (col < 10 - 1 && *(*(arr + fila + 1) + col + 1) == -1)
+                    contador++;
+                if (col > 0 && *(*(arr + fila + 1) + col - 1) == -1)
+                    contador++;
+            }
+            if (col < 10 - 1 && *(i + 1) == -1)
+                contador++;
+            if (col > 0 && *(i - 1) == -1)
+                contador++;
+
+            *i = contador;
+        }
+    }
+}
+
+int main() {
+
+
+    int x;
+    cout << "Ingresa la cantidad de numeros aleatorios (maximo 10): ";
+    cin >> x;
+
+    int contador = 1;
+    while (contador <= x) {
+        int (*q)[10] = arr + rand() % 10;
+        int* z = *q + rand() % 10;
+        *z = -1; // mina
+        contador++;
+    }
+
+    thread t1(cuentaporfilas, 0, 10 / 2);
+    thread t2(cuentaporfilas, 10 / 2, 10);
+
+    t1.join();
+    t2.join();
+
+    for (int (*p)[10] = arr; p < arr + 10; p++) {
+        for (int* q = *p; q < *(p + 1); q++) {
+            cout << *q << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

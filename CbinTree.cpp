2279a50cd3900ed944675b4780d2824da403fab3
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2025-09-21 14:54:56 -0500

    Update CbinTree.cpp

--- CbinTree.cpp
+++ CbinTree.cpp
@@ -16,15 +16,17 @@ struct Node
 class ArbolBinario
 {
 public:
+	Node* tail;
+
 	ArbolBinario();
 
 	bool find(int x , Node**& pos);
 	bool insert(int x);
 	bool del(int x);
+	void print(Node* n);
 	~ArbolBinario();
 
 private:
-	Node* tail;
 };
 
 ArbolBinario::ArbolBinario()
@@ -61,9 +63,87 @@ bool ArbolBinario::insert(int x)
 
 bool ArbolBinario::del(int x)
 {
-	return false;
+	Node** pos = nullptr;
+	if (!find(x,pos))
+	{
+		return false;
+	}
+	Node* nod = *pos;
+	//primer caso , nodos hoja
+	if (nod->der == nullptr && nod->izq == nullptr)
+	{
+		*pos = nullptr;
+		delete nod;
+		return true;
+	}
+	//segundo caso, nodos con un hijo
+	if (nod->der == nullptr)
+	{
+		*pos = nod->der;
+		delete nod;
+		return true;
+	}
+
+	if (nod->izq == nullptr)
+	{
+		*pos = nod->izq;
+		delete nod;
+		return true;
+	}	
+	//tercer caso , nodos con dos hijos
+
+	Node** tmp_2 = &(*pos)->der;
+	while ( (*tmp_2)->izq != nullptr )
+	{
+		tmp_2 = &(*tmp_2)->izq;
+	}
+	nod->x = (*tmp_2)->x;
+	Node* tmp_3 = *tmp_2;
+	*tmp_2 = (*tmp_2)->der; // aqui siempre a la derecha , pq puede haber un caso donde el menor nodo del sub arbol izquierda tenga solo un hijo pero no dos.
+	delete tmp_3;
+	return true;
+}
+
+void ArbolBinario::print(Node* n)
+{
+	if (n == nullptr) {
+		return;
+	}
+	if (n->izq) {
+		print(n->izq);
+	}
+	cout << n->x << " ";
+	if (n->der) {
+		print(n->der);
+	}
 }
 
 ArbolBinario::~ArbolBinario()
 {
+	delete tail;
+}
+int main() {
+	ArbolBinario a;
+	a.insert(52);
+	a.insert(41);
+	a.insert(64);
+	a.insert(55);
+	a.insert(32);
+	a.insert(44);
+	a.insert(73);
+	a.insert(56);
+
+	Node* temp = a.tail;
+	a.print(temp);
+	cout << endl;
+
+	a.del(52);
+	a.print(temp);
+	cout << endl;
+
+	a.del(44);
+	a.print(temp);
+	cout << endl;
+
+	return 0;
 }

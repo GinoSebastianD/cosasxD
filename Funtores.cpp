283f692c51558239b31d4539fa11c20fca3dfb83
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2025-10-01 20:20:39 -0500

    Create Funtores.cpp

---
+++ Funtores.cpp
@@ -0,0 +1,46 @@
+//#include "iostream"
+//using namespace std;
+//
+//template<class T , int n = 0>
+//class Suma
+//{
+//public:
+//
+//	T val = n;
+//	//suma(T x):val(x) {};
+//	int operator () (int x) {
+//		return n + x;
+//	}
+//};
+//
+//template<class O>
+//class eje
+//{
+//public:
+//	
+//	O operador;
+//};
+//
+//int main() {
+//
+//	Suma<int> st;
+//	cout << st(5) << " ";
+//	eje<Suma<int, 15>> nt;
+//	cout << nt.operador(20) << " ";
+//}
+
+#include "iostream"
+using namespace std;
+
+template<class T>
+class Suma
+{
+public:
+	Suma();
+	~Suma();
+
+private:
+
+};
+
+

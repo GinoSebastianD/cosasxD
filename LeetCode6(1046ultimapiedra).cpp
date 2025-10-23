8861dbb0c253013221028cdeeded12c9d62e7f0a
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2025-10-22 21:57:36 -0500

    Create LeetCode6(1046ultimapiedra).cpp

---
+++ LeetCode6(1046ultimapiedra).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class piedra
+{
+public:
+
+	int ultima(vector<int>& stones);
+private:
+
+};
+int piedra::ultima(vector<int>& stones)
+{
+	
+	return 0;
+}
+
+
+
+int main() {
+	piedra pi;
+	vector<int> vec = {2,7,4,1,8,1};
+	pi.ultima(vec);
+}
+

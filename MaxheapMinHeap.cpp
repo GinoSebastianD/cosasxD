a46c6010869e06d75027fbf87596c8b9cbedef01
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2025-10-26 13:48:17 -0500

    Add pop and arriba methods to heap class
    Implemented pop and arriba methods for heap management.

--- MaxheapMinHeap.cpp
+++ MaxheapMinHeap.cpp
@@ -24,6 +24,8 @@ public:
 	void push(int x);
 	void heap_max(Node* root);
 	bool find(int x,Node* root);
+	int pop();
+	void arriba(Node* node);
 	void print();
 private:
 	Node* root;
@@ -46,6 +48,71 @@ bool heap::find(int x, Node* root)
 		return true;
 	}
 	return find(x, root->nodes[0]) || find(x, root->nodes[1]);
+}
+
+int heap::pop()
+{
+	if (!root) return -1;
+
+	int maxValue = root->v;
+	Node* lastNode = root;
+
+	deque<Node*> q;
+	q.push_back(root);
+	while (!q.empty()) {
+		lastNode = q.front();
+		q.pop_front();
+		if (lastNode->nodes[0]) q.push_back(lastNode->nodes[0]);
+		if (lastNode->nodes[1]) q.push_back(lastNode->nodes[1]);
+	}
+
+	if (root != lastNode) {
+		root->v = lastNode->v;
+
+		Node* parent = lastNode->padre;
+		if (parent->nodes[0] == lastNode) {
+			parent->nodes[0] = nullptr;
+		}
+		else {
+			parent->nodes[1] = nullptr;
+			col.push_front(parent);
+		}
+	}
+	else {
+		root = nullptr;
+		while (!col.empty()) {
+			col.pop_front();
+		}
+	}
+
+	delete lastNode;
+	arriba(root);
+	return maxValue;
+}
+
+void heap::arriba(Node* node)
+{
+	while (node) {
+		Node* maxChild = nullptr;
+		if (node->nodes[0] && node->nodes[1]) {
+			maxChild = node->nodes[0]->v > node->nodes[1]->v ?
+				node->nodes[0] : node->nodes[1];
+		}
+		else if (node->nodes[0]) {
+			maxChild = node->nodes[0];
+		}
+		else if (node->nodes[1]) {
+			maxChild = node->nodes[1];
+		}
+
+		if (maxChild && maxChild->v > node->v) {
+			swap(node->v, maxChild->v);
+			node = maxChild;
+		}
+		else {
+			break;
+		}
+	}
 }
 
 void heap::print()
@@ -111,8 +178,10 @@ void heap::heap_max(Node* root)
 
 int main() {
 	heap hmax;
-	for (int i = 1; i <= 10; ++i) {
+	for (int i = 1; i <= 10; i++)
+	{
 		hmax.push(i);
 	}
+	cout << hmax.pop() << endl;;
 	hmax.print();
 }

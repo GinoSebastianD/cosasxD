a89cad7926b81d522de13bffb06350a561e0fc73
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2025-11-11 19:15:58 -0500

    Create TablaHash.cpp

---
+++ TablaHash.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <list>
+#include<vector>
+
+struct cf {
+    int operator()(int x) const {
+        return x;
+    }
+};
+
+// tipo de dato, tipo de ED, functionObjects, tamaño del vector
+template<class T, class C , class F , int S>
+class TableHash
+{
+public:
+	TableHash() {
+		ht = new std::vector<C>(S);
+	};
+
+	F f;
+	std::vector<C>* ht;
+
+	void ins(T x);
+
+};
+
+template<class T, class C, class F, int S>
+void TableHash<T, C, F, S>::ins(T x)
+{
+    int i = f(x) % S;
+    (*ht)[i].push_front(x);
+}
+
+
+
+int main() {
+	TableHash<int, std::list<int>, cf, 7> ht;
+	ht.ins(7);
+	ht.ins(8);
+	ht.ins(10);
+	ht.ins(11);
+	ht.ins(5);
+
+}
